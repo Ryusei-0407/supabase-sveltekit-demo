2d32c64bb049961123bb3677d4c92955259ec67c
ryu <ryu000407@gmail.com>
2022-05-20 14:11:16 +0900

    update: add path alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import path from 'path';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+export default {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					$components: path.resolve('./src/components')
+				}
+			}
+		}
 	}
 };
-
-export default config;
